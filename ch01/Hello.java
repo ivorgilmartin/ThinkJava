4dc6d120a769946fdf080a4fa9d2ceb58cd429dc
ivorgilmartin <50912513+ivorgilmartin@users.noreply.github.com>
2020-09-15 10:03:41 +0100

    adding ch01

---
+++ ch01/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+		// comment
+    public static void main(String[] args) {
+        // generate some simple output
+        System.out.println("Hello, World! How are you?");
+    }
+
+}
