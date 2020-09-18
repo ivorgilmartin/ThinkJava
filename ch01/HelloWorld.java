35f98e22c985d913ecd88b7652ad5aa7289972b6
ivorgilmartin <50912513+ivorgilmartin@users.noreply.github.com>
2020-09-18 15:27:51 +0100

    deleted files

--- ch01/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    
-    public static void main(String[] args) {
-        // generate some simple output
-        System.out.println("Hello, World!"); // first line
-	System.out.println("How are you?");  // another line
-    }
-
-}
