3a3c0a9d7ab9cc8a165d297a52bba70d28fc662d
ivorgilmartin <50912513+ivorgilmartin@users.noreply.github.com>
2020-09-18 15:31:32 +0100

    updated tabs

---
+++ ch01/HelloWorld.java
@@ -0,0 +1,10 @@
+package ie.gmit.dip;
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // generate some simple output
+        System.out.println("Hello, World!"); // first line
+        System.out.println("How are you?");  // another line
+    }
+
+}
