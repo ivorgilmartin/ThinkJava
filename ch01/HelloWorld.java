eb703acc83be06ba8c6e157acbb39099abfb7325
ivorgilmartin <50912513+ivorgilmartin@users.noreply.github.com>
2020-09-20 16:21:46 +0100

    small change

--- ch01/HelloWorld.java
+++ ch01/HelloWorld.java
@@ -1,9 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // generate some simple output
-        System.out.println("Hello, World!"); // first line
-        System.out.println("How are you?");  // another line
+        System.out.println("Hello, World!");
+        System.out.println("How are you?");  // second line
     }
 
 }
